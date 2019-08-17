91dfd62c999b95e929a6e553c87f82392cc8eb95
mjvr <mjvr@mjvrs-MacBook-Pro.local>
2019-08-17 16:33:31 +0200

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(document).scroll(function() {
+        var nav = $(".my-navbar")
+        nav.toggleClass('scrolled', $(this).scrollTop() > nav.height())
+    })
+})
