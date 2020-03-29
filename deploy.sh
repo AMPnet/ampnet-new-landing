0ee98004582da7084fa2983cff79aa33eb11e668
mj-ampnet <mislav@ampnet.io>
2020-03-29 20:55:06 +0200

    color changes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 ssh root@ampnet.io "rm -rf /var/www/ampnet-landing/; mkdir /var/www/ampnet-landing" &&
-scp -r ../ampnet-landing/* root@ampnet.io:/var/www/ampnet-landing/ 
+scp -r ../ampnet-new-landing/* root@ampnet.io:/var/www/ampnet-landing/ 
