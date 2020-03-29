ba9fe1af856b012a957edbecbe98e8fb24e45378
mj-ampnet <mislav@ampnet.io>
2020-03-29 18:58:24 +0200

    update

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
         var nav = $(".my-navbar")
         nav.toggleClass('scrolled', $(this).scrollTop() > nav.height())
     })
+    
 })
+
+window.onload = function() {
+	lax.setup() // init
+
+	const updateLax = () => {
+		lax.update(window.scrollY)
+		window.requestAnimationFrame(updateLax)
+	}
+
+	window.requestAnimationFrame(updateLax)
+}
