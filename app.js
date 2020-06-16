2c34abc5aec3c5ef14a7eb49419f3b8e7b53ea8d
shamanotjungle <shamanotjungle@shamanotjungles-MacBook-Pro.local>
2020-06-16 13:48:16 +0200

    update

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-$(document).ready(function() {
-    $(document).scroll(function() {
-        var nav = $(".my-navbar")
-        nav.toggleClass('scrolled', $(this).scrollTop() > nav.height())
-    })
-    
+$(document).ready(function () {
+	$(document).scroll(function () {
+		var nav = $(".my-navbar")
+		nav.toggleClass('scrolled', $(this).scrollTop() > nav.height())
+	})
+
+	var myform = $("form#contact-form");
+	myform.submit(function (event) {
+		event.preventDefault();
+
+		// Change to your service ID, or keep using the default service
+		var service_id = "default_service";
+		var template_id = "request_quote";
+
+		myform.find("button").text("Sending...");
+		emailjs.sendForm(service_id, template_id, myform[0])
+			.then(function () {
+				alert("Sent!");
+				myform.find("button").text("Send");
+			}, function (err) {
+				alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+				myform.find("button").text("Send");
+			});
+		return false;
+	});
+
 })
 
-window.onload = function() {
+window.onload = function () {
 	lax.setup() // init
 
 	const updateLax = () => {
@@ -16,3 +36,4 @@ window.onload = function() {
 
 	window.requestAnimationFrame(updateLax)
 }
+
